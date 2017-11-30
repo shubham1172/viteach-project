49407a0043142a918e9bf4713c010aa6d56a1e36
shubham1172 <shubhamsharma1172@gmail.com>
2017-11-30 22:18:29 +0530

    Updated visuals

---
+++ plot_last_schools.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%      Plot last schools of each cluster   %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('clusterdata.mat'); % data and time_routes imported
+
+%% FINAL DATA
+KITCHEN_X = [20.840249];
+KITCHEN_Y = [73.065424];
+figure;
+hold on;
+test = 1;
+plot(KITCHEN_X, KITCHEN_Y, 'MarkerSize', 10, 'Marker', '*', 'Color', 'b')
+for i = 2:length(data),
+  if(data(i,1)~=data(i-1,1))
+    plot(data(i-1,2), data(i-1,3), 'MarkerSize', 10, 'Marker', '*', 'Color', 'r');
+    disp(test);
+    test = 1;
+  else
+    test = test+1;
+  end
+end
+plot(data(end,2), data(end,3), 'MarkerSize', 10, 'Marker', '*', 'Color', 'r');
+axis([20.5 21.1 72.7 73.5]); % select custom axis scale
+set(gca,'Color','k');
--- visualize.m
+++ visualize.m
@@ -9,12 +9,18 @@ KITCHEN_Y = [73.065424];
 
 %% COLOR MAPS
 colormap = [hot(15); winter(15); summer(15); cool(15)];
-randRows = randi(size(colormap, 1), [28, 1]);
+randRows = randi(size(colormap, 1), [28, 1]); 
 randCols = colormap(randRows, :);
 %% PLOT
 plot(KITCHEN_X, KITCHEN_Y, 'Color', 'k', 'Marker', 'd');
 hold on
-for i = 1:length(data)
-  plot(data(i,2), data(i,3), 'Color', randCols(data(i,1), :), 'Marker', '*');
+for i = 2:length(data)
+  if(data(i,1)~=data(i-1,1))
+    marker='*'; markerSize=10;
+  else
+    marker='+'; markerSize=5;
+  end
+  plot(data(i,2), data(i,3), 'Color', randCols(data(i,1), :), 'Marker', marker, 'MarkerSize', markerSize);
 end
+plot(data(end,2), data(end,3), 'Color', randCols(data(end,1), :), 'Marker', '*', 'MarkerSize', 10);
 axis([20.5 21.1 72.7 73.5]); % select custom axis scale
